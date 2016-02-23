3d510eb8f9efdd600f29610e8d7ce52648ec40b4
Natasha <natasha.sha.shaaa@gmail.com>
2016-02-23 00:13:16 -0500

    updated formatting/spacing

--- js/main.js
+++ js/main.js
@@ -1,43 +1,40 @@
 $(document).ready(function() {
 
-	$('section').waypoint(function(direction) {
+//this waypoint updates the navigation scrolling up
+  $('section').waypoint(function(direction) {
+    var location,
+        activeSection = $(this.element);
 
-		var location,
-				activeSection = $(this.element);
-
-		if(direction === 'down'){
-    	activeSection = $(this.element).next();
+    if(direction === 'down'){
+      activeSection = $(this.element).next();
     } 
     
     activeSection = $(this.element);
 
     location = activeSection.attr('id');
 
-    // console.log('location : '+location)
-    // console.log('direction : '+direction)
-		$('nav div a').removeClass('active');
+    $('nav div a').removeClass('active');
 
-		$('div [data-label="'+location+'"] a').addClass('active');
+    $('div [data-label="'+location+'"] a').addClass('active');
 
-	},{offset : '70%'});
+  },{offset : '70%'});
 
-	$('section').waypoint(function(direction) {
+//this waypoint updates the navigation scrolling down
+  $('section').waypoint(function(direction) {
+    var location,
+        activeSection = $(this.element);
 
-		var location,
-				activeSection = $(this.element);
-
-  	if (direction === 'up'){
-    	activeSection = $(this.element).prev();
+    if (direction === 'up'){
+      activeSection = $(this.element).prev();
     }
     
     activeSection = $(this.element);
 
     location = activeSection.attr('id');
 
-		$('nav div a').removeClass('active');
+    $('nav div a').removeClass('active');
 
-		$('div [data-label="'+location+'"] a').addClass('active');
+    $('div [data-label="'+location+'"] a').addClass('active');
 
-	},{offset : 'bottom-in-view'});
-	// {offset:'-60%'}
+  },{offset : 'bottom-in-view'});
 });
